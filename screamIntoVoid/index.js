f2c68210c00555f81daff9793433db2e0eeb6fea
l337hax0r <lonesealpro@gmail.com>
2021-03-15 16:47:23 -0700

    bot works but needs refactoring

--- screamIntoVoid/index.js
+++ screamIntoVoid/index.js
@@ -1,9 +1,11 @@
 //requires the dotenv file where token is located and configures it.
 const dotenv = require("dotenv").config();
+const { prefix, token } = require("./config.json");
 //requires the discord.js module so we can use discord.js methods and modules
 const Discord = require("discord.js");
 //creates a new discord client
 const client = new Discord.Client();
+const fs = require("fs");
 
 //when the client is ready, run this code
 //this event will only trigger one time after logging in
@@ -11,5 +13,15 @@ client.once("ready", () => {
   console.log("Ready!");
 });
 
+client.on("message", async (message) => {
+  // Join the same voice channel of the author of the message
+  if (message.member.voice.channel) {
+    const connection = await message.member.voice.channel.join();
+    // From a path
+    connection.play("./scream1.mp3");
+  }
+  connection.disconnect();
+});
+
 //login to your discord with your apps token
-client.login(process.env.TOKEN);
+client.login(token);
